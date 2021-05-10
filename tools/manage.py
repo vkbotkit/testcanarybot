34c4e5f4b2addf14a78586590aa3b252f3c6ecc4
Андрей Прокофьев <65781959+kensoi@users.noreply.github.com>
2021-05-10 18:47:09 +0500

    Update manage.py

--- tools/manage.py
+++ tools/manage.py
@@ -1,11 +1,11 @@
-# Packaet wrapper
+# TPPM wrapper
 # Copyright 2021 Andrew Prokofieff
 
-# works with projects that's has generated root.py file via packaet
+# works with projects that's has generated root.py file via TPPM
 # put this file into folder where you located all your projects. It shall look like this:
 
 # your_dir/
-#   manage.py <-- packaet wrapper
+#   manage.py <-- TPPM wrapper
 #   botName1/
 #     assets/
 #     library/
@@ -20,5 +20,5 @@
 
 import subprocess
 
-projects = ['kyokou', 'kyokou2'] # list of projects by project folder name
+projects = ['botName1', 'botName2'] # list of projects by project folder name
 subprocess.run(f'python -m testcanarybot --run {" ".join(projects)}'.split())
